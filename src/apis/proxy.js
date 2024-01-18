6c0244d6b108864884aa9e88e049cef864248c58
JOE <86701548+zxytt@users.noreply.github.com>
2024-01-18 11:29:58 +0800

    feat: 'vercel跨域配置'

--- src/apis/proxy.js
+++ src/apis/proxy.js
@@ -1,8 +1,6 @@
 // 该服务为 vercel serve跨域处理
-const {
-  createProxyMiddleware
-  } = require('http-proxy-middleware')
-  module.exports = (req, res) => {
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = (req, res) => {
   let target = ''
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
@@ -11,11 +9,11 @@ const {
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
-  target,
-  changeOrigin: true,
-  pathRewrite: {
-  // 通过路径重写，去除请求路径中的 `/api`
-  '^/api/': '/'
-  }
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+    // 通过路径重写，去除请求路径中的 `/api`
+      '^/api/': '/'
+    }
   })(req, res)
-  }
+}
